dcc06b95b4393652afdbc6c06be71dd0e6814234
Tatsuya-Nonogaki <winfield@support.email.ne.jp>
2025-02-22 13:57:39 +0900

    Base upload files

---
+++ export-ADData.ps1
@@ -0,0 +1,223 @@
+<#
+ .SYNOPSIS
+  Exports group and users from Active Directory.
+
+ .DESCRIPTION
+  Exports group and users from Active Directory to CSV files.
+  Version: 0.7.10
+
+ .PARAMETER DNPrefix
+  (Alias -d) Mandatory. Mutually exclusive with DNPath. 
+  Domain component from which you want to retrieve
+  objects. For example: "unit.mydomain.local" which is converted to 
+  DistinguishedName(DNPath) "OU=unit,DC=mydomain,DC=local" in the script.
+
+ .PARAMETER DCDepth
+  Optional. Mutually exclusive with DNPath. 
+  In calculation of the DNPath, we assume the last 2 elements are DC 
+  per default. If it is not what you expect, specify depth count of 
+  DC with this. e.g., when -DNPath dept.unit.mydomain.local, then
+   DCDepth 2: DNPath becomes OU=dept,OU=unit,DC=mydomain,DC=local
+   DCDepth 3: DNPath becomes OU=dept,DC=unit,DC=mydomain,DC=local
+  -DCDepth 4: DNPath becomes DC=dept,DC=unit,DC=mydomain,DC=local
+
+ .PARAMETER DNPath
+  (Alias -p) Optional. Mutually exclusive with DNPrefix and DCDepth. 
+  Instead of specifying DNPrefix (optionally with DCDepth), you can 
+  explicitly specify it in DistinguishedName format. This is preferable 
+  for accuracy, if you are familiar with DN expression of AD components. 
+
+ .PARAMETER OutPath
+  (Alias -o) Optional. Folder path where you want to save output CSV files.
+  Path selection diablog will ask you if omitted.
+#>
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [Alias("p")]
+    [string]$DNPath,
+
+    [Parameter()]
+    [Alias("d")]
+    [string]$DNPrefix,
+
+    [Parameter()]
+    [int]$DCDepth = 2,
+
+    [Parameter()]
+    [Alias("o")]
+    [string]$OutPath
+)
+
+begin {
+    Import-Module ActiveDirectory -ErrorAction Stop
+
+    # Arguments validation
+    if ($PSBoundParameters.Count -eq 0) {
+        Get-Help $MyInvocation.InvocationName
+        exit
+    }
+
+    if (-not $PSBoundParameters.ContainsKey('DNPath')) {
+        if ($PSBoundParameters.ContainsKey('DCDepth') -and -not $PSBoundParameters.ContainsKey('DNPrefix')) {
+            throw "Error: -DNPrefix must be specified when using -DCDepth."
+        }
+        if (-not $PSBoundParameters.ContainsKey('DNPrefix')) {
+            throw "Error: One of -DNPath or -DNPrefix is required."
+        }
+    }
+
+    if ($PSBoundParameters.ContainsKey('DNPath') -and ($PSBoundParameters.ContainsKey('DNPrefix') -or $PSBoundParameters.ContainsKey('DCDepth'))) {
+        throw "Error: -DNPath cannot be used together with -DNPrefix or -DCDepth."
+    }
+}
+
+process {
+
+    $scriptdir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+    # Folder selection dialog
+    function Get-Folder {
+        Add-Type -AssemblyName System.Windows.Forms
+        $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+        $folderBrowser.Description = "Select the output folder"
+        $folderBrowser.SelectedPath = $scriptdir
+        if ($folderBrowser.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+            $selectedPath = $folderBrowser.SelectedPath
+            return $selectedPath
+        }
+        return ""
+    }
+
+    # Generate DN path from DN prefix
+    function ConvertPrefixToDNPath {
+        param (
+            [string]$prefix,
+            [int]$DCDepth
+        )
+        $domainParts = $prefix.Split('.')
+        if ($domainParts.Count -lt 2) {
+            Write-Error "Invalid prefix format: Expected at least two domain components (e.g., mydomain.local)"
+            exit 1
+        }
+        if ($DCDepth -lt 1 -or $DCDepth -gt $domainParts.Count) {
+            Write-Error "Invalid DCDepth: It must be at least 1 and at most the total number of domain components."
+            exit 1
+        }
+
+        $DNPath = ""
+
+        # Assume DC are the last DCDepth elements
+        $dcParts = $domainParts[-$DCDepth..-1]
+
+        # Assume the shalower elements are OU
+        $ouParts = $domainParts[0..($domainParts.Count - $DCDepth - 1)]
+
+        foreach ($ou in [array]::Reverse($ouParts)) {
+            $DNPath += "OU=$ou,"
+        }
+
+        foreach ($dc in $dcParts) {
+            $DNPath += "DC=$dc,"
+        }
+
+        return $DNPath.TrimEnd(',')
+    }
+
+    # Get output folder path
+    function Select-OutputFolderPath {
+        $outputFolderPath = ""
+        if (-not $OutPath) {
+            $outputFolderPath = Get-Folder
+        } else {
+            $outputFolderPath = $OutPath
+        }
+        if (-not $outputFolderPath) {
+            Write-Error "Output folder path is not specified"
+            exit 1
+        }
+        return $outputFolderPath
+    }
+
+    # Check existence of the DN Path on the AD
+    function Check-DNPathExistence {
+        param ([string]$DNPath)
+
+        # Validate DNPath format
+        if ($DNPath -notmatch "^(CN|OU|DC)=[^,]+(,(CN|OU|DC)=[^,]+)*$") {
+            Write-Error "Invalid DN format: $DNPath"
+            return $false
+        }
+
+        # Check if DNPath exists in AD
+        try {
+            $exists = [adsi]::Exists("LDAP://$DNPath")
+        } catch {
+            return $false
+        }
+        return $exists
+    }
+
+    #
+    ## Main
+    #
+
+    # Determine DNPath based on argument combination
+    if ($PSBoundParameters.ContainsKey('DNPrefix')) {
+        if ($DCDepth -lt 2) {
+            Write-Error "Invalid DCDepth: $DCDepth. It must be 2 or greater"
+            exit 1
+        }
+
+        $DNPath = ConvertPrefixToDNPath -prefix $DNPrefix -DCDepth $DCDepth
+        if (-not $DNPath) {
+            Write-Error "Error occurred while converting DNPrefix to DNPath"
+            exit 1
+        }
+    }
+
+    if (-not (Check-DNPathExistence -DNPath $DNPath)) {
+        Write-Error "Invalid or non-existent DNPath: $DNPath"
+        exit 1
+    }
+
+    write-host "DN Path = $DNPath"
+
+    $outputFolderPath = Select-OutputFolderPath
+
+    if (-not $DNPrefix) {
+        $dnParts = $DNPath -split ',' | ForEach-Object { $_ -replace '^(OU=|DC=)', '' }
+        if ($dnParts.Count -eq 0) {
+            Write-Error "Failed to extract domain name from DNPath: $DNPath"
+            exit 1
+        }
+        $domain = ($dnParts -join '_')
+    } else {
+        $domain = $DNPrefix.Replace('.', '_')
+    }
+
+    $userOutputFilePath = $outputFolderPath + "\Users_" + $domain + ".csv"
+    $groupOutputFilePath = $outputFolderPath + "\Groups_" + $domain + ".csv"
+
+    write-host "User Output File Path = $userOutputFilePath"
+    write-host "Group Output File Path = $groupOutputFilePath"
+
+    # Additional user properties we want to include in output
+    $userExtraProps = "MemberOf", "EmailAddress", "HomePhone", "MobilePhone", "OfficePhone", "Title", "Department", "Manager", "LockedOut", "*"
+
+    # Store Manager property value as a DistinguishedName so that it can be easily given to import script
+    Get-ADUser -Filter * -Properties $userExtraProps -SearchBase "$DNPath" | 
+      Select-Object @{Name="MemberOf"; Expression={$_.MemberOf -join ";"}}, `
+                    @{Name="Manager"; Expression={ if ($_.Manager) { (Get-ADUser -Identity $_.Manager).DistinguishedName } else { $null } }}, `
+                    * -ExcludeProperty MemberOf, Manager | 
+        Export-Csv -Path $userOutputFilePath -Encoding UTF8 -NoTypeInformation
+
+    # Additional group properties we want to include in output
+    $groupExtraProps = "MemberOf", "ManagedBy", "*"
+
+    Get-ADGroup -Filter * -Properties $groupExtraProps -SearchBase "$DNPath" | 
+     Select-Object @{Name="MemberOf"; Expression={$_.MemberOf -join ";"}}, * -ExcludeProperty MemberOf |
+      Export-Csv -Path $groupOutputFilePath -Encoding UTF8 -NoTypeInformation
+
+# End of process
+}
