f5cf76dbabb53cb9ec0b9b35df380d05e2ec2b86
Tatsuya-Nonogaki <winfield@support.email.ne.jp>
2025-03-17 20:11:00 +0900

    Update compare-ADCSV.ps1
    Some code cleaning

--- compare-ADCSV.ps1
+++ compare-ADCSV.ps1
@@ -5,7 +5,7 @@
  .DESCRIPTION
   Compare two CSVs of users or groups exported from Active Directory, 
   with sAMAccountName as the key.
-  Version: 0.1.1
+  Version: 0.1.2
  
  .PARAMETER OldFile
   (Alias -o) Mandatory. Old CSV file to compare, with relative or absolute path.
@@ -14,8 +14,8 @@
   (Alias -n) Mandatory. New CSV file, with relative or absolute path.
  
  .PARAMETER OutFile
-  Optional. Path of desired output CSV file. If not specified, output is written 
-  only to the PS console.
+  Optional. Path to a CSV file for the output. If not specified, output 
+  is written only to the PS console.
 #>
 [CmdletBinding()]
 param(
@@ -30,8 +30,6 @@ param(
     [Parameter(Position=2)]
     [string]$OutFile
 )
-
-$scriptdir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
 
 $oldUsers = Import-Csv -Path $OldFile
 $newUsers = Import-Csv -Path $NewFile
@@ -66,7 +64,7 @@ foreach ($oldUser in $oldUsers) {
             $comparisonResults += $comparisonResult
         }
     } else {
-        # User present in old file but missing in new file
+        # Entries present in old file but missing in new file
         $comparisonResult = [PSCustomObject]@{
             sAMAccountName     = $sAMAccountName
             OldDistinguishedName = $oldUser.DistinguishedName
@@ -77,14 +75,13 @@ foreach ($oldUser in $oldUsers) {
     }
 }
 
-# Check for users present in new file but missing in old file
+# Check for entries present in new file but missing in old file
 $oldUsersHashTable = @{}
 $oldUsers | ForEach-Object { $oldUsersHashTable[$_.sAMAccountName] = $_ }
 
 foreach ($newUser in $newUsers) {
     $sAMAccountName = $newUser.sAMAccountName
     if (-not $oldUsersHashTable.ContainsKey($sAMAccountName)) {
-        # User present in new file but missing in old file
         $comparisonResult = [PSCustomObject]@{
             sAMAccountName     = $sAMAccountName
             OldDistinguishedName = "MISSING"
