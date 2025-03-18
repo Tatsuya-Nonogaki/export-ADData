06b72712a9e340a3def1679578cfe904b5f73b17
Tatsuya-Nonogaki <winfield@support.email.ne.jp>
2025-03-19 00:56:30 +0900

    Added Enabled/PasswordNeverExpires to compare : compare-ADCSV.ps1

--- compare-ADCSV.ps1
+++ compare-ADCSV.ps1
@@ -5,12 +5,12 @@
  .DESCRIPTION
   Compare two CSVs of users or groups exported from Active Directory, 
   with sAMAccountName as the key.
-  Version: 0.1.3
+  Version: 0.1.4
  
  .PARAMETER OldFile
   (Alias -o) Mandatory. Old CSV file to compare, with relative or absolute path.
  
- .PARAMETER OldFile
+ .PARAMETER NewFile
   (Alias -n) Mandatory. New CSV file, with relative or absolute path.
  
  .PARAMETER OutFile
@@ -42,6 +42,14 @@ $newUsers = Import-Csv -Path $NewFile
 
 $comparisonResults = @()
 
+# Determine if the CSVs are user exports by checking for user-specific properties
+$isUserExport = $false
+if ($oldUsers[0].PSObject.Properties.Name -contains 'ObjectClass') {
+    $isUserExport = $oldUsers[0].ObjectClass -eq 'user'
+} else {
+    $isUserExport = $oldUsers[0].PSObject.Properties.Name -contains 'GivenName' -or $oldUsers[0].PSObject.Properties.Name -contains 'Surname' -or $oldUsers[0].PSObject.Properties.Name -contains 'UserPrincipalName'
+}
+
 $newUsersHashTable = @{}
 $newUsers | ForEach-Object { $newUsersHashTable[$_.sAMAccountName] = $_ }
 
@@ -52,30 +60,34 @@ foreach ($oldUser in $oldUsers) {
         $oldDN = $oldUser.DistinguishedName
         $newDN = $newUser.DistinguishedName
 
-        if ($oldDN -ne $newDN) {
-            $differencePoints = Compare-Object -ReferenceObject ($oldDN -split ',') -DifferenceObject ($newDN -split ',') | ForEach-Object {
-                if ($_.SideIndicator -eq '<=') {
-                    "--- $($_.InputObject)"
-                } elseif ($_.SideIndicator -eq '=>') {
-                    "+++ $($_.InputObject)"
-                }
+        $differencePoints = Compare-Object -ReferenceObject ($oldDN -split ',') -DifferenceObject ($newDN -split ',') | ForEach-Object {
+            if ($_.SideIndicator -eq '<=') {
+                "--- $($_.InputObject)"
+            } elseif ($_.SideIndicator -eq '=>') {
+                "+++ $($_.InputObject)"
             }
-
-            $comparisonResult = [PSCustomObject]@{
-                sAMAccountName     = $sAMAccountName
-                OldDistinguishedName = $oldDN
-                NewDistinguishedName = $newDN
-                DifferencePoints    = $differencePoints -join "; "
-            }
-            $comparisonResults += $comparisonResult
         }
-        elseif ($IncludeEqual) {
-            $comparisonResult = [PSCustomObject]@{
-                sAMAccountName     = $sAMAccountName
-                OldDistinguishedName = $oldDN
-                NewDistinguishedName = $newDN
-                DifferencePoints    = ""
-            }
+
+        $comparisonResult = [PSCustomObject]@{
+            sAMAccountName     = $sAMAccountName
+            OldDistinguishedName = $oldDN
+            NewDistinguishedName = $newDN
+            DifferencePoints    = $differencePoints -join "; "
+        }
+
+        if ($isUserExport) {
+            $oldEnabled = if ($oldUser.PSObject.Properties.Name -contains 'Enabled') { $oldUser.Enabled } else { "" }
+            $newEnabled = if ($newUser.PSObject.Properties.Name -contains 'Enabled') { $newUser.Enabled } else { "" }
+            $oldPasswordNeverExpires = if ($oldUser.PSObject.Properties.Name -contains 'PasswordNeverExpires') { $oldUser.PasswordNeverExpires } else { "" }
+            $newPasswordNeverExpires = if ($newUser.PSObject.Properties.Name -contains 'PasswordNeverExpires') { $newUser.PasswordNeverExpires } else { "" }
+
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name OldEnabled -Value $oldEnabled
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name NewEnabled -Value $newEnabled
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name OldPasswordNeverExpires -Value $oldPasswordNeverExpires
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name NewPasswordNeverExpires -Value $newPasswordNeverExpires
+        }
+
+        if ($comparisonResult.DifferencePoints -or ($isUserExport -and ($comparisonResult.OldEnabled -ne $comparisonResult.NewEnabled -or $comparisonResult.OldPasswordNeverExpires -ne $comparisonResult.NewPasswordNeverExpires)) -or $IncludeEqual) {
             $comparisonResults += $comparisonResult
         }
     } else {
@@ -86,6 +98,19 @@ foreach ($oldUser in $oldUsers) {
             NewDistinguishedName = "MISSING"
             DifferencePoints    = "Entry missing in new CSV"
         }
+
+        if ($isUserExport) {
+            $oldEnabled = if ($oldUser.PSObject.Properties.Name -contains 'Enabled') { $oldUser.Enabled } else { "" }
+            $newEnabled = ""
+            $oldPasswordNeverExpires = if ($oldUser.PSObject.Properties.Name -contains 'PasswordNeverExpires') { $oldUser.PasswordNeverExpires } else { "" }
+            $newPasswordNeverExpires = ""
+
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name OldEnabled -Value $oldEnabled
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name NewEnabled -Value $newEnabled
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name OldPasswordNeverExpires -Value $oldPasswordNeverExpires
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name NewPasswordNeverExpires -Value $newPasswordNeverExpires
+        }
+
         $comparisonResults += $comparisonResult
     }
 }
@@ -103,6 +128,19 @@ foreach ($newUser in $newUsers) {
             NewDistinguishedName = $newUser.DistinguishedName
             DifferencePoints    = "Entry missing in old CSV"
         }
+
+        if ($isUserExport) {
+            $oldEnabled = ""
+            $newEnabled = if ($newUser.PSObject.Properties.Name -contains 'Enabled') { $newUser.Enabled } else { "" }
+            $oldPasswordNeverExpires = ""
+            $newPasswordNeverExpires = if ($newUser.PSObject.Properties.Name -contains 'PasswordNeverExpires') { $newUser.PasswordNeverExpires } else { "" }
+
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name OldEnabled -Value $oldEnabled
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name NewEnabled -Value $newEnabled
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name OldPasswordNeverExpires -Value $oldPasswordNeverExpires
+            $comparisonResult | Add-Member -MemberType NoteProperty -Name NewPasswordNeverExpires -Value $newPasswordNeverExpires
+        }
+
         $comparisonResults += $comparisonResult
     }
 }
